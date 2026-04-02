b38cbdf0c12d832f6982b7788a170a449a741a2e
vladwb <vladwithcode@gmail.com>
2026-04-02 16:10:17 -0600

    chore: add components (and form deps)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
         remotePatterns: [new URL("https://lh3.googleusercontent.com/aida-public")],
-    }
+    },
+    output: "standalone",
+    basePath: isProduction ? "/terrenos/circuito-emperador-i" : "",
 };
 
 export default nextConfig;
